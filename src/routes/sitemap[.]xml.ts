c03347d57a14887e8e649dcec2c6134279cd9d0e
FIRST_NAME LAST_NAME <acaleyam06@gmail.com>
2026-08-01 14:17:41 +0200

    Cambio de dominio

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://aljucen-country-escape.lovable.app";
+const BASE_URL = "https://casa-rural-la-plata.lovable.app/";
 
 interface SitemapEntry {
   path: string;
